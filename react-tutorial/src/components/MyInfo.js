4c5170f28f4b5928b006f67f23ec461672d37bfc
bcwan <bcwan8@gmail.com>
2019-07-31 14:53:31 -0700

    added components folder

---
+++ react-tutorial/src/components/MyInfo.js
@@ -0,0 +1,19 @@
+import React from "react" // import this everytime use JSX
+
+function MyInfo() {
+  return <div>
+          <h1>
+            Name: Brian
+          </h1>
+          <p>
+            I'm a new software engineer. Working for a year now in the industry.
+          </p>
+          <ul>
+            <li>Korea</li>
+            <li>China</li>
+            <li>Japan</li>
+          </ul>
+         </div>
+}
+
+export default MyInfo //for exporting to allow other files to access
--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -3,39 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-
-// functional components
-function MyApp() {
-  return <div>
-            <h1>
-            Finance
-            </h1>
-            <ul>
-              <li>ETF</li>
-              <li>Dividend</li>
-              <li>Mutual Funds</li>
-            </ul>
-          </div>
-}
-
-function MyInfo() {
-  return <div>
-          <h1>
-            Name: Brian
-          </h1>
-          <p>
-            I'm a new software engineer. Working for a year now in the industry.
-          </p>
-          <ul>
-            <li>Korea</li>
-            <li>China</li>
-            <li>Japan</li>
-          </ul>
-         </div>
-}
+import MyInfo from "./components/MyInfo.js"
 
 //JSX
 ReactDOM.render(<MyInfo />, document.getElementById('root'));
-
-var myNewP = document.createElement('p');
-myNewP.innerHTML = "This is a paragraph."
