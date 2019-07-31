5d20df3d1e1f943fd1a41ac36b468ea91e7d9c5b
bcwan <bcwan8@gmail.com>
2019-07-31 14:44:14 -0700

    added additional functional changes

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-
+// functional components
 function MyApp() {
   return <div>
             <h1>
@@ -18,8 +18,24 @@ function MyApp() {
           </div>
 }
 
+function MyInfo() {
+  return <div>
+          <h1>
+            Name: Brian
+          </h1>
+          <p>
+            I'm a new software engineer. Working for a year now in the industry.
+          </p>
+          <ul>
+            <li>Korea</li>
+            <li>China</li>
+            <li>Japan</li>
+          </ul>
+         </div>
+}
+
 //JSX
-ReactDOM.render(<MyApp />, document.getElementById('root'));
+ReactDOM.render(<MyInfo />, document.getElementById('root'));
 
 var myNewP = document.createElement('p');
 myNewP.innerHTML = "This is a paragraph."
