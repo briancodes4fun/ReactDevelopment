342771722f1d20051b93bfd7b776aff4f996e78c
bcwan <bcwan8@gmail.com>
2019-07-31 15:31:56 -0700

    adding new React app

---
+++ parent-child-component/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Footer from "./Footer"
+
+function App() {
+  return (
+    <div>
+        <nav>
+            <h1>
+              Hello a third time!
+            </h1>
+            <ul>
+              <li>Korea</li>
+              <li>China</li>
+              <li>Japan</li>
+            </ul>
+        </nav>
+        <main>
+          <p>This is where most of my content will go...</p>
+        </main>
+        <Footer />
+    </div>
+  )
+
+
+}
+
+export default App;
